a056c4b847a3c12a14f3693a245e4fb9d3aca3b6
Ben Millwood <benmachine+git@mercenariesguild.net>
2009-04-16 15:07:34 +0100

    Add inet_pton's docstring and match the prototype

--- utils.py
+++ utils.py
@@ -5,14 +5,18 @@ try:
     # available we'll just have to use mine
     from socket import inet_pton
 except ImportError:
-    def inet_pton(af, addr):
+    def inet_pton(af, ip):
+        '''inet_pton(af, ip) -> packed IP address string
+
+        Convert an IP address from string format to a packed string suitable
+        for use with low-level network functions.'''
         if af == AF_INET:
             try:
-                return ''.join(chr(int(b)) for b in addr.split('.'))
+                return ''.join(chr(int(b)) for b in ip.split('.'))
             except ValueError:
                 raise sockerr('illegal IP address string passed to inet_pton')
         elif af == AF_INET6:
-            bits = addr.split('::')
+            bits = ip.split('::')
             if len(bits) > 2:
                 raise sockerr('illegal IP address string passed to inet_pton')
             try:
@@ -24,7 +28,7 @@ except ImportError:
                     full += [0 for _ in range(8 - len(lead) - len(trail))]
                     full += [int(s, 16) for s in trail]
                 else:
-                    full = [int(s, 16) for s in addr.split(':')]
+                    full = [int(s, 16) for s in ip.split(':')]
             except ValueError:
                 raise sockerr('illegal IP address string passed to inet_pton')
             return ''.join([chr(b >> 8) + chr(b & 0xff) for b in full])
