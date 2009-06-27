cc332dcf795bbca9a950af798409d2fb83978540
Ben Millwood <benmachine+git@mercenariesguild.net>
2009-06-27 02:58:39 +0100

    Replace WithTDB with contextlib.closing

--- db.py
+++ db.py
@@ -2,24 +2,17 @@
 # Copyright (c) Ben Millwood 2009
 # This file is part of the Tremulous Master server.
 
+from contextlib import closing
 from os import O_RDWR, O_CREAT
 from tdb import Tdb
 
 from config import log, LOG_VERBOSE, LOG_DEBUG
 
-class WithTDB(Tdb):
-    '''A TDB file that can be used as the expression of a `with' statement
-    in a similar way to normal files'''
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.close()
-
 def log_client(addr, info):
     try:
         # TODO: check if flags are necessary
-        with WithTDB('clientStats.tdb', flags = O_RDWR|O_CREAT) as database:
+        with closing(Tdb('clientStats.tdb',
+                         flags = O_RDWR|O_CREAT)) as database:
             try:
                 version = info['version']
                 renderer = info['renderer']
