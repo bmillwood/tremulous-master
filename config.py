68f1f0295c70baedb940e3b9fc65d75295c7aff5
Ben Millwood <benmachine+git@mercenariesguild.net>
2009-06-27 17:18:09 +0100

    Remove superfluous testing code

--- config.py
+++ config.py
@@ -389,8 +389,3 @@ class MasterConfig:
 
 config = MasterConfig()
 log = config.log
-
-if __name__ == '__main__':
-    # This is useful with python -i config.py to examine a newly-created
-    # configuration.
-    config = MasterConfig()
