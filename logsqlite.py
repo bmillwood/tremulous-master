872eda9386ee7e734d4e30e613ac79ef2323fff4
Ben Millwood <benmachine+git@mercenariesguild.net>
2010-01-23 18:00:20 +0000

    Use UTF-8 for stats/database

--- logsqlite.py
+++ logsqlite.py
@@ -1,36 +1,31 @@
 #!/usr/bin/env python
 from sqlite3 import connect
 from time import time
+from functools import partial
 from hashlib import md5
 
 db_id = 'Using SQLite database backend'
 
+unicise = partial(unicode, encoding = 'utf-8')
+
 def log_client(addr, info):
-    renderer = info['renderer']
-    version = info['version']
-    for c in renderer + version:
-        if ord(c) & 0x80:
-            raise ValueError('client infostring contained non-ASCII character')
+    renderer = unicise(info['renderer'])
+    version = unicise(info['version'])
     with connect('stats.db') as db:
         dbc = db.cursor()
-        dbc.execute('INSERT INTO clients VALUES(?, ?, ?)',
-                    (addr.host, renderer, version))
+        dbc.execute('INSERT INTO clients (addr, version, renderer) '
+                    'VALUES (?, ?, ?)',
+                    (addr.host, version, renderer))
 
 def log_gamestat(addr, data):
-    for c in data:
-        if ord(c) & 0x80:
-            raise ValueError('gamestat data contained non-ASCII character')
     with connect('stats.db') as db:
         dbc = db.cursor()
-        dbc.execute('INSERT INTO gamestats VALUES(?, ?, ?)',
-                    (addr.host, int(time()), data))
+        dbc.execute('INSERT INTO gamestats (addr, time, data) '
+                    'VALUES (?, ?, ?)',
+                    (addr.host, int(time()), unicise(data)))
 
-def create_db():
-    from sys import argv, exit
-    if len(argv) == 1:
-        exit('Usage: {0} FILE'.format(argv[0]))
-    assert len(argv) > 1
-    with connect('stats.db') as db:
+def create_db(path):
+    with connect(path) as db:
         dbc = db.cursor()
         dbc.execute('CREATE TABLE IF NOT EXISTS '
                     'clients (addr TEXT, version TEXT, renderer TEXT)')
@@ -38,4 +33,11 @@ def create_db():
                     'gamestats (addr TEXT, time INTEGER, data TEXT)')
 
 if __name__ == '__main__':
-    create_db()
+    from sys import argv, exit
+    if len(argv) > 1:
+        exit('Usage: python {0} [file]'.format(argv[0]))
+    try:
+        p = argv[1]
+    except IndexError:
+        p = 'stats.db'
+    create_db(p)
